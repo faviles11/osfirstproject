0a647293c75816acfa2043e15b87cb61a86a95f8
Camila Pastor Castro <153691227+CamilaPastor@users.noreply.github.com>
2025-04-26 19:18:40 -0600

    Update broker.c

--- src/broker.c
+++ src/broker.c
@@ -7,12 +7,23 @@
 // TCP logic
 #include <netinet/in.h>
 #include <arpa/inet.h>
+//****#include <semaphore.h> incluir libreria en caso que se usen semaforos 
+
 
 // Port where the broker is going to listen
 #define PORT 12345         
 // Maximum number of pending connections        
 #define MAX_CLIENTS 10
 #define BUFFER_SIZE 1024
+
+
+//****ESTRUCTURA PARA REPRESENTAR GRUPO DE CONSUMIDORES (CON UN SEMAFORO) 
+//*****ESTRUCTURA PARA ALMACENAR INFORMACÓN DE GRUPOS Y OFFSETS
+//******AÑADIR CAMPO PARA ALMACENAR UN OFFSET 
+
+/*SE PUEDE USAR TRYLOCK AL ENVIAR MENSAJES
+DONDE EL EL PRODUCTOR ENVÌA MESNAJES A LOS CONSUMIDORES 
+SE USA pthread_mutex_trylock() PARA ADQUIRIR EL MUTEX DEL CONSUMIDOR ANTES DE ENVIAR*/
 
 // executed per each client's thread that connects to the broke
 // socket_desc -> represents the client's socket
@@ -21,6 +32,8 @@ void* handle_client(void* socket_desc) {
     // local buffer to store the data received from the client
     char buffer[BUFFER_SIZE];
 
+//*****ASOCIARLE AL CONSUMIDOR UN OFFSET INICIAL 
+    
     // while the client is connected
     while (1) {
         // cleans the buffer
@@ -33,6 +46,12 @@ void* handle_client(void* socket_desc) {
         printf("Broker recieved: %s", buffer);
         // TODO
         // CONSUMER GROUP LOGIC
+
+    /*COMPARANDO EL OFFSET DEL CONSUMIDOR CON EL OFFSET DEL 
+        MENSAJE ACTUAL PARA ENVIAR LOS MENSAJES QUE EL CONSUMIDOR
+        AUN NO HA RECIBIDO. DESPUES DE ENVIAR EL MENSAJE A UN CONSUMIDOR
+        SE DEBE ACTUALIZAR SU OFFSET.*/
+        
     }
     close(sock);
 
@@ -49,6 +68,10 @@ int main() {
     // size of the struct
     int addrlen = sizeof(address);
 
+/*INICIALIZAR SEMFORO*/
+/*INICIALIZAR MUTEX O ESTRUCTURA SI SE USA PARA PROTEGER 
+EL ACCESO  CONCURRENTE A LAS ESTRUCTURAS */
+    
     // creates socket TCP in IPV4 (AF_INET)
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     // IPv4
@@ -77,7 +100,20 @@ int main() {
 
         // releases resources use by the thread
         pthread_detach(t);
+
+        /*DESTRUIR EL SEMAFORO AL FINALIZAR EL PROGRAMA */
+/*LIBERAR CUALQUIER MUTEX O MEMORIA QUE SE HAYA INICIALIZADO 
+*/
     }
 
     return 0;
 }
+
+
+/*pthread_mutex_trylock() se puede usar cuando se necesita acceder
+a un recurso protegido por un mutex
+Se puede controlar el acceso a la infomacion de cada consumidor
+como en el offset al enviar mensajes.
+MEJORA LA CAPACIDAD DE RESPUESTA DEL BROKER YA QUE PERMITE QUE EL PRODUCTOR 
+INTENTE ENVIAR MENSAJES A LOS CONSUMIDORES SIN BLOQUEARSE EN CASO QUE UN CONSUMIDOR ESTÉ OCUPADO
+*/
